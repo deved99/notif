376c5c754c8b4b2ffa5e0be9221bf8033b181869
Davide Forlani <forla.df@gmail.com>
2023-03-27 20:53:56 +0200

    Bugfix.

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,14 @@
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use rusqlite::Connection;
 
 use crate::Result;
 
-const DB_PATH: &'static str = "./notif.sqlite3";
-
 pub fn get_connection() -> Result<Connection> {
-    let path_exists = get_database_path().exists();
-    let connection = Connection::open(DB_PATH)?;
+    let path = get_database_path();
+    let path_exists = Path::new(&path).exists();
+    let connection = Connection::open(&path)?;
     if !path_exists {
         create_tables(&connection)?;
     }
@@ -34,10 +33,9 @@ pub fn create_tables(connection: &Connection) -> Result<()> {
     Ok(())
 }
 
-fn get_database_path() -> PathBuf {
-    let path = match env::var("XDG_DATA_DIR") {
+fn get_database_path() -> String {
+    match env::var("XDG_DATA_DIR") {
         Ok(s) => format!("{}/notif.sqlite3", s),
         Err(_) => format!("{}/.local/share/notif.sqlite3", env::var("HOME").unwrap()),
-    };
-    Path::new(&path).to_owned()
+    }
 }
