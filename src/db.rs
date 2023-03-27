2ca0846e9c0c7f7ff5937d16e91834872339cd47
Davide Forlani <forla.df@gmail.com>
2023-03-27 19:42:52 +0200

    Changed sqlite path.

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,5 @@
-use std::path::Path;
+use std::env;
+use std::path::{Path, PathBuf};
 
 use rusqlite::Connection;
 
@@ -7,7 +8,7 @@ use crate::Result;
 const DB_PATH: &'static str = "./notif.sqlite3";
 
 pub fn get_connection() -> Result<Connection> {
-    let path_exists = Path::new(DB_PATH).exists();
+    let path_exists = get_database_path().exists();
     let connection = Connection::open(DB_PATH)?;
     if !path_exists {
         create_tables(&connection)?;
@@ -33,3 +34,10 @@ pub fn create_tables(connection: &Connection) -> Result<()> {
     Ok(())
 }
 
+fn get_database_path() -> PathBuf {
+    let path = match env::var("XDG_DATA_DIR") {
+        Ok(s) => format!("{}/notif.sqlite3", s),
+        Err(_) => format!("{}/.local/share/notif.sqlite3", env::var("HOME").unwrap()),
+    };
+    Path::new(&path).to_owned()
+}
